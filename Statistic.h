b15a3d6f9865ccee5aabf246664bfd7495ef04f7
Mark Hsu <mark50404@gmail.com>
2019-08-15 15:46:19 +0800

    first version, global best, local worst

---
+++ Statistic.h
@@ -0,0 +1,16 @@
+//
+// Created by Mark Hsu on 2019-08-12.
+//
+
+#ifndef QTS_KNAPSACK_STATISTIC_H
+#define QTS_KNAPSACK_STATISTIC_H
+
+#define STATISTICAL_ROUND 10 // how many times you wanna execute QTS
+
+bool isOverweight(int);
+
+void statistic();
+
+void finalResult();
+
+#endif //QTS_KNAPSACK_STATISTIC_H
---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include "QTS.h"
+#include "Statistic.h"
+#include <iostream>
+#include <time.h>
+
+
+using namespace std;
+
+int main() {
+    auto start = chrono::steady_clock::now();// count execution time
+
+    for (int i = 0; i < STATISTICAL_ROUND; i++) {
+        cout << "Round " << (i + 1) << "/" << STATISTICAL_ROUND << endl;
+        init();
+
+        for (int j = 0; j < ROUND; j++) {
+            measure();
+            calcFitness();
+            update();
+//            prtPMatrix();
+        }
+
+#if EACH_ROUND_RESULT
+        showResult();
+#endif
+
+        statistic();
+    }
+
+    finalResult();
+
+    // count execution time
+    auto end = chrono::steady_clock::now();
+    cout << "Time taken: " << chrono::duration<double>(end - start).count() << " s" << endl;
+    return 0;
+}
